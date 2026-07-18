0729855f3dd8995658734ef31aa2262c53617978
Murilo Silveira <murilorms22@gmail.com>
2026-07-18 15:09:31 -0300

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${outfit.variable} h-full antialiased scroll-smooth`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P29929TD');`,
+          }}
+        />
+      </head>
+      <body className="min-h-full flex flex-col">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P29929TD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
